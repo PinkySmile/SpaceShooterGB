8a265402f769375bf50c18afc23e70c6c49fb025
Andgel <andgel.halley@epitech.eu>
2020-02-21 19:38:55 +0100

    More cleaning

--- src/main.asm
+++ src/main.asm
@@ -38,14 +38,6 @@ testSGB::
 	xor b
 	ret
 
-; The text displayed when playing on a Gameboy
-onlyGBCtext::
-	db "You need a   ", " ",          "                  "
-	db "Gameboy Color", TM_CHARACTER, " or a             "
-	db "Super Gameboy", TM_CHARACTER, "                  "
-	db "to run this."
-onlyGBCtextEnd:
-
 ; Main function
 main::
 	call init               ; Init
@@ -58,6 +50,7 @@ main::
 ; Runs the main program
 run::
 	ld hl, $FF42
+	call drawBackground
 .gameLoop:
 	halt
 	dec [hl]
